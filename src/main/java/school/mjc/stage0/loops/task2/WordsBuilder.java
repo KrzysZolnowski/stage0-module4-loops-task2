4eb385ae80a11bf0cebb189d0f520100439d4de2
Krzysztof <krzys.zolnowski@gmail.com>
2024-08-14 13:13:39 +0200

    add seven task done

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+        StringBuilder phrase = new StringBuilder();
+        for (char c : chars) {
+            phrase.append(c);
+        }
+        System.out.println(phrase.toString());
+    }
 
+    public static void main(String[] args) {
+        WordsBuilder builder = new WordsBuilder();
+        builder.buildPhrase('H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!');
     }
 }
+
+
+
