e1331eddfb5f72e9fa8ef491e577b86fc2bdb9db
Krzysztof <krzys.zolnowski@gmail.com>
2024-08-14 12:09:33 +0200

    add forth task done

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int number = 0;
 
+        while (printToInclusive >= 1) {
+            System.out.println(printToInclusive);
+            printToInclusive -=1;
+        }
     }
 }
