240edf9f55b219050489ba8e6e4ab3599e56b7d4
Krzysztof <krzys.zolnowski@gmail.com>
2024-08-14 12:22:00 +0200

    add fifth task done (and fixed)

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -3,9 +3,13 @@ package school.mjc.stage0.loops.task2;
 public class PowerOfTwo {
     public void printPower(int power) {
         int force = 0;
-        while (force <=power) {
-            System.out.println(2 ^ force);
-            force += 1;
+        while (force <= power) {
+            if (power > 0) {
+                System.out.println(2 ^ force);
+                force += 1;
+            } else {
+                System.out.println("too much power");
+            }
         }
     }
 }
